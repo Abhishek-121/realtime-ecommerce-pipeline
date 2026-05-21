3ce10d56a0ae6478331da300b3353c291c5830f7
Abhishek Maheshwari <abhishek69744mahesheshwari@gmail.com>
2026-05-21 16:53:05 +0530

    Added the code for invalid order and in consumer push the invalid record to failed_orders topic

--- consumer/order_consumer.py
+++ consumer/order_consumer.py
@@ -1,6 +1,6 @@
 import json
 
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 
 consumer = KafkaConsumer(
     "orders",
@@ -10,49 +10,31 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-dlq_producer = KafkaProducer(
+producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-required_fields = [
-    "order_id",
-    "user_id",
-    "amount"
-]
+required_fields = ["order_id", "user_id", "amount"]
 
 for message in consumer:
-
     order = message.value
-
     print(f"Received Event: {order}")
-
-     # Validation Logic
     missing_fields = []
-
     for field in required_fields:
-
         if field not in order:
             missing_fields.append(field)
 
     # If fields missing -> send to DLQ
     if missing_fields:
-
         print(f"Invalid Event. Missing fields: {missing_fields}")
-
         dlq_payload = {
             "failed_event": order,
             "missing_fields": missing_fields,
             "reason": "Schema validation failed"
         }
-
-        dlq_producer.send(
-            "failed_orders",
-            value=dlq_payload
-        )
-
-        print("Sent to DLQ Topic")
-
+        producer.send("failed_orders",value=dlq_payload)
     else:
-        print("Valid Event Processed Successfully")
+        producer.send("processed_orders", value=order)
+        print("Event Processed Successfully to processed_order topic...")
 
--- producer/order_producer.py
+++ producer/order_producer.py
@@ -2,12 +2,10 @@ import json
 import random
 import time
 from datetime import datetime
-
 from faker import Faker
 from kafka import KafkaProducer
 
 fake = Faker()
-
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
@@ -30,18 +28,16 @@ CITIES = [
 ]
 
 while True:
-
     order = {
         "order_id": fake.uuid4(),
         "user_id": random.randint(1000, 9999),
         "product": random.choice(PRODUCTS),
         "amount": round(random.uniform(100, 5000), 2),
-        "city": random.choice(CITIES),
         "event_time": datetime.utcnow().isoformat()
     }
-
+    # Introduce bad records intentionally
+    if random.randint(1, 5) == 1:
+        del order["user_id"] #user_id is required field
     producer.send("orders", value=order)
-
     print(f"Produced: {order}")
-
     time.sleep(2)
